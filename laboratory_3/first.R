c6ea2d02c4c54fd0359a405c50314b73e15a8a3c
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-10-31 14:19:47 +0200

    Added the variance estimator function.

--- laboratory_3/first.R
+++ laboratory_3/first.R
@@ -9,25 +9,42 @@ b Estimate Var(U1 + U2) and Var(U1) + Var(U2). Are they equal?
 Should the true values be equal?
 c Estimate P(U1 + U2 ≤ 1.5).
 """
-find_mean <- function(U1, U2) {
+compare_means <- function(U1, U2) {
   # E(U1 + U2)
   first_mean <- mean(U1 + U2)
   # E(U1) + E(U2)
   second_mean <- mean(U1) + mean(U2)
-  cat(" E(U1 + U2):", first_mean, "\n", "E(U1) + E(U2):", second_mean, "\n", "uniform distributon: (0,1)\n")
+  cat("\n E(U1 + U2):", first_mean, "\n", "E(U1) + E(U2):", second_mean, "\n", "uniform distributon: (0,1)\n")
   
   if (first_mean > 1) {
-    cat("The estimate", first_mean, "is larger than the true value.\n")
+    cat(" The estimate", first_mean, "is larger than the true value.\n")
   } else {
-    cat("The estimate", first_mean, "is smaller than the true value.\n")
+    cat(" The estimate", first_mean, "is smaller than the true value.\n")
   }
   
   if(first_mean == second_mean) {
-    cat("The estimate of E[U1+U2] and E[U1]+E[U2] are equal.\n")
+    cat(" The estimate of E[U1+U2] and E[U1]+E[U2] are equal.\n")
   } else if (first_mean > second_mean) {
-    cat("The estimate of E[U1+U2] is larger than E[U1]+E[U2].\n")
+    cat(" The estimate of E[U1+U2] is larger than E[U1]+E[U2].\n")
   } else {
-    cat("The estimate of E[U1+U2] is smaller than E[U1]+E[U2].\n")
+    cat(" The estimate of E[U1+U2] is smaller than E[U1]+E[U2].\n")
+  }
+  
+}
+
+compare_variances <- function(U1, U2) {
+  #Var(U1 + U2)
+  first_variance <- var(U1 + U2)
+  #Var(U1) + Var(U2)
+  second_variance <- var(U1) + var(U2)
+  cat(" \n Var(U1 + U2):", first_variance, "\n", "Var(U1) + Var(U2):", second_variance, "\n", "uniform distributon: (0,1)\n")
+  
+  if(first_variance == second_variance){
+    cat(" The estimate of Var[U1+U2] and Var[U1]+Var[U2] are equal.\n")
+  }else if (first_variance > second_variance){
+    cat(" The estimate of Var[U1+U2] is larger than Var[U1]+Var[U2].\n")
+  }else{
+    cat(" The estimate of Var[U1+U2] is smaller than Var[U1]+Var[U2].\n")
   }
   
 }
@@ -35,7 +52,8 @@ find_mean <- function(U1, U2) {
 first_main <- function() {
   U1 <- runif(10000, min=0, max=1);  
   U2 <- runif(10000, min=0, max=1);
-  find_mean(U1, U2)
+  compare_means(U1, U2)
+  compare_variances(U1, U2)
 }
 
 first_main()
